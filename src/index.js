a7c5568dcc8958a55566ac1554e770f08bb2e47b
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2021-11-23 19:26:58 -0400

    Fixed some code errors within the code

--- src/index.js
+++ src/index.js
@@ -114,4 +114,3 @@ var getRandomQuote = function () {
     return (quotes[Math.floor(Math.random() * (quotes.length + 1))]["quote"] + ' ~ ' + quotes[Math.floor(Math.random() * (quotes.length + 1))]["author"]);
 };
 exports.getRandomQuote = getRandomQuote;
-console.log((0, exports.getRandomQuote)());
